60b6e6d13cfae266ce72ab716b8b0a83ae341fc6
Missy <mjoycabral0503@gmail.com>
2019-04-22 13:14:05 -0500

    -a

--- assets/javascript.js
+++ assets/javascript.js
@@ -25,25 +25,60 @@ $("button").on("click", function displayGifs () {
 
         var personImage = $("<img>");
         personImage.attr("src", results[i].images.fixed_height.url);
+        personImage.attr("data-animate", results[i].images.fixed_height_still.url);
+        personImage.attr("data-still", results[i].images.fixed_height.url);
+
         gifDiv.prepend(p);
         gifDiv.prepend(personImage);
 
         $("#gifs-appear-here").prepend(gifDiv);
     }
     });
+});
 
-    $(document).on("click", "#character", displayGifs);
-    $(document).on("click", "#buttons", function () {
+// $(document).on("click", "#character", displayGifs);
+$(document).on("click", "img", function () {
     var state = $(this).attr("data-state");
+    console.log(state);
+    console.log($(this).attr("data-animate"));
     if (state == "still"){
-        $(this).attr("src", $(this).data("animate"));
+        $(this).attr("src", $(this).attr("data-animate"));
         $(this).attr("data-state", "animate");
 
     } else {
-        $(this).attr("src", $(this).data("still"));
+        $(this).attr("src", $(this).attr("data-still"));
         $(this).attr("data-state", "still");
     }
-    }
-);
 });
 
+$("#add-character").on("click", function(event) {
+    event.preventDefault();
+    console.log ("hello");
+    var character = $("#character-input").val().trim();
+    if (character == ""){
+        return false;
+    }
+    characters.push(character);
+
+    // displayGifButton();
+    var newButton=$("<button>");
+    newButton.text(character);
+    newButton.attr("data-person", character);
+
+    $("#buttons").append(newButton);
+
+    return false;
+});
+
+    // function addNewButton () {
+    //     $("#add-character").on("click", function() {
+    //         var character = $("character-input").val().trim();
+    //         if (character == ""){
+    //             return false;
+    //         }
+    //         topic.push(character);
+
+    //         displayGifButton();
+    //         return false;
+    //     });
+    // }
